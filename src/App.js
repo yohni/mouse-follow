37156cd64b13a63c13a7bd30be58242e5945bf22
yohni <yohni.123@gmail.com>
2022-07-05 22:01:58 +0700

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { ThemeProvider } from '@mui/material/styles'
 import { Route, Routes } from 'react-router-dom'
 import MyFace from './containers/MyFace'
 import Home from './Home'
 import PageXY from './PageXY'
+import theme from './theme'
 
 function App() {
   return (
     <div className="app">
-      <Routes>
-        <Route path="xy" element={<PageXY />} />
-        <Route path="/my-face" element={<MyFace />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="xy" element={<PageXY />} />
+          <Route path="/my-face" element={<MyFace />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   )
 }
---
+++ src/theme/index.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#515151',
+    },
+  },
+})
+
+export default theme
